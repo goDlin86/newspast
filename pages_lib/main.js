bf4cab0db3420e47852eaf60b763d0f416542a69
goDlin <godlin086@gmail.com>
2021-02-17 20:25:24 +0300

    more loading

--- pages_lib/main.js
+++ pages_lib/main.js
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.css'
 
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 import Item from './item'
 
@@ -10,16 +11,15 @@ import 'dayjs/locale/ru'
 dayjs.locale('ru')
 
 const Main = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState({ afterDate: '', after: '', news: [] })
     const { theme } = useParams()
 
     useEffect(() => {
+        //setData([])
         fetchData()
-    }, [])
+    }, [theme])
 
     const fetchData = async () => {
-        //setData([])
-
         try {
             const data = JSON.stringify({ theme: theme, after: data ? data.after : '', afterDate: data ? data.afterDate : '' })
 
@@ -31,10 +31,8 @@ const Main = () => {
                 n.date = dayjs(n.date).format('DD MMM YYYY HH:MM')
                 return n
             })
-            
-            const d = { 'afterDate': results.afterDate, 'after': results.after, news }
 
-            setData(d)
+            setData({ afterDate: results.afterDate, after: results.after, news: data.news.concat(news) })
 
         } catch(err) {
             console.error(err)
@@ -45,8 +43,23 @@ const Main = () => {
         <main className={styles.main}>
           <div className={styles.theme}>{theme}</div>
           <div className={styles.news}>
-            {data.length === 0 && <div>Загрузка...</div>}
-            {data.news && data.news.map((item, i) => <Item item={item} key={i} />)}
+            {data.length === 0 && <p>Загрузка...</p>}
+            {data.news && 
+                <InfiniteScroll 
+                    dataLength={data.news.length}
+                    next={fetchData}
+                    hasMore={true}
+                    style={{'overflow': 'unset'}}
+                    scrollThreshold={0.95}
+                    loader={<p>Загрузка...</p>}
+                    endMessage={
+                        <p style={{textAlign: 'center'}}>
+                        <b>Больше нет</b>
+                        </p>
+                    }>
+                    {data.news.map((item, i) => <Item item={item} key={i} />)}
+                </InfiniteScroll>
+            }
           </div>
         </main>
     )
