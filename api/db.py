9423c7376dfa4d8a02a3f620e595febbe657a0ef
goDlin <godlin086@gmail.com>
2021-02-18 18:39:19 +0300

    update api

--- api/db.py
+++ api/db.py
@@ -17,16 +17,35 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         m = pymorphy2.MorphAnalyzer()
+        client = FaunaClient(secret=os.environ.get('DBSECRET'))
+        futr_news = []
 
-        theme = 'nation'
+        getNews('world')
+        getNews('nation')
+        getNews('scitech')
+
+        if len(futr_news) > 0:
+            client.query(
+                q.map_(
+                    lambda post: q.create(
+                        q.collection('NewsPast'),
+                        {'data': post}
+                    ),
+                    futr_news
+                ))
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(futr_news).encode())
+        return
+
+    def getNews(theme):
         url = 'https://news.google.com/news/rss/headlines/section/topic/'+theme.upper()+'.ru_ru/?ned=ru_ru&hl=ru'
 
         r = requests.get(url)
         xml = etree.XML(r.content)
 
-        client = FaunaClient(secret=os.environ.get('DBSECRET'))
-
-        futr_news = []
         news = []
         for item in xml.find('channel').iterfind('item'):
             t = item.find('title').text
@@ -59,19 +78,3 @@ class handler(BaseHTTPRequestHandler):
                             futr_news.append(n)
                 except:
                     pass
-
-        if len(futr_news) > 0:
-            client.query(
-                q.map_(
-                    lambda post: q.create(
-                        q.collection('NewsPast'),
-                        {'data': post}
-                    ),
-                    futr_news
-                ))
-
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(futr_news).encode())
-        return
