7d0bbb63d95403a767b5a49fcdc5d94fa48fd4f2
goDlin <godlin086@gmail.com>
2021-02-25 19:21:36 +0300

    let after

--- pages_lib/main.js
+++ pages_lib/main.js
@@ -11,21 +11,23 @@ import 'dayjs/locale/ru'
 dayjs.locale('ru')
 
 const Main = () => {
-    const [data, setData] = useState({ news: [], after: '', afterDate: '' })
+    const [news, setNews] = useState([])
+    let [after, setAfter] = useState({ after: '', afterDate: '' })
     const [hasMore, setHasMore] = useState(true)
     const { theme } = useParams()
 
     useEffect(() => {
-        setData({ news: [], after: '', afterDate: '' })
+        setAfter({ after: '', afterDate: '' })
+        setNews([])
         setHasMore(true)
-        fetchData(true)
+        fetchData()
     }, [theme])
 
-    const fetchData = async (first = false) => {
+    const fetchData = async () => {
         try {
             const res = await fetch('/api/getNews', { 
                 method: 'POST', 
-                body: JSON.stringify({ theme, after: first ? '' : data.after, afterDate: first ? '' : data.afterDate }) 
+                body: JSON.stringify({ theme, after: after.after, afterDate: after.afterDate })
             })
             const json = await res.json()
             const results = json || []
@@ -43,7 +45,8 @@ const Main = () => {
                 return n
             })
 
-            setData(prevState => ({ news: prevState.news.concat(news), after: results.after, afterDate: results.afterDate }))
+            setNews(prevState => prevState.concat(news))
+            setAfter({ after: results.after, afterDate: results.afterDate })
 
         } catch(err) {
             console.error(err)
@@ -55,7 +58,7 @@ const Main = () => {
         <main className={styles.main}>
             <div className={styles.theme}>{theme}</div>
             <InfiniteScroll 
-                dataLength={data.news.length}
+                dataLength={news.length}
                 next={fetchData}
                 hasMore={hasMore}
                 style={{'overflow': 'unset'}}
@@ -71,7 +74,7 @@ const Main = () => {
                         <b>Больше нет</b>
                     </p>
                 }>
-                {data.news.map((item, i) => <Item item={item} key={i} />)}
+                {news.map((item, i) => <Item item={item} key={i} />)}
             </InfiniteScroll>
         </main>
     )
