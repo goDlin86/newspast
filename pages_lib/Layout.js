829fbfaca659b7742019dd7626e40bcddf4f7a5a
goDlin <godlin086@gmail.com>
2021-09-10 17:14:57 +0300

    fix apple

--- pages_lib/Layout.js
+++ pages_lib/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
             <Head>
                 <title>NEWS from the PAST</title>
                 <link rel='icon' href='/favicon.ico' />
-                <link rel='pple-touch-icon' sizes='180x180' href='/apple-touch-icon-180.png' />
+                <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon-180.png' />
                 <link href='https://fonts.googleapis.com/css?family=Lato:100,400,700,900' rel='stylesheet' type='text/css' />
             </Head>
 
