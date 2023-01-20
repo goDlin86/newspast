3e6f047c5c657ef3d53944cc246f674914f17a07
Denis <godlin086@gmail.com>
2023-01-20 12:35:45 +0300

    add 2023

--- app/loading.js
+++ app/loading.js
@@ -2,6 +2,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Loading() {
     return (
-        <div className={styles.main}>Загрузка...</div>
+        <div className={styles.main}>
+            <b>Загрузка...</b>
+        </div>
     )
 }
