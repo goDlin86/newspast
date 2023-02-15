84fffa0d800377176275cce3db8389552c95ae82
Denis <godlin086@gmail.com>
2023-02-15 17:25:35 +0300

    fix inngest

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -15,9 +15,9 @@ const getNews = inngest.createFunction(
   }
 )
 
-const get = async (theme) => {
-  const res = await fetch('https://newspast.vercel.app/api/db?theme=' + theme)// + '&secret=' + process.env.SECRET_TOKEN)
-  return await res.json()
-}
+// const get = async (theme) => {
+//   const res = await fetch('https://newspast.vercel.app/api/db?theme=' + theme)// + '&secret=' + process.env.SECRET_TOKEN)
+//   return await res.json()
+// }
 
 export default serve(inngest, [ getNews ])
