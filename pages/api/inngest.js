520b3fc9222067960dafb85ec711de2824a89669
Denis <godlin086@gmail.com>
2023-02-15 17:19:47 +0300

    try inngest

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -7,9 +7,11 @@ const getNews = inngest.createFunction(
   { name: 'Get news' }, 
   { cron: '0 */6 * * *' }, 
   async ({ event, step }) => {
-    await step.run('db world', async () => { return await get('world') })
-    await step.run('db nation', async () => { return await get('nation') })
-    await step.run('db scitech', async () => { return await get('scitech') })
+    // await step.run('db world', async () => { return await get('world') })
+    // await step.run('db nation', async () => { return await get('nation') })
+    // await step.run('db scitech', async () => { return await get('scitech') })
+    const res = await fetch('https://newspast.vercel.app/api/db?theme=world')// + '&secret=' + process.env.SECRET_TOKEN)
+    return await res.json()
   }
 )
 
