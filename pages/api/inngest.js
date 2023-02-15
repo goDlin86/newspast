06d5a71f6eb11c99bd250af6c464431260e9bbd8
Denis <godlin086@gmail.com>
2023-02-15 17:30:49 +0300

    add inngest integration

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -7,17 +7,15 @@ const getNews = inngest.createFunction(
   { name: 'Get news' }, 
   { cron: '0 */6 * * *' }, 
   async ({ event, step }) => {
-    // await step.run('db world', async () => { return await get('world') })
-    // await step.run('db nation', async () => { return await get('nation') })
-    // await step.run('db scitech', async () => { return await get('scitech') })
-    const res = await fetch('https://newspast.vercel.app/api/db?theme=world')// + '&secret=' + process.env.SECRET_TOKEN)
-    return await res.json()
+    await step.run('db world', async () => { return await get('world') })
+    await step.run('db nation', async () => { return await get('nation') })
+    await step.run('db scitech', async () => { return await get('scitech') })
   }
 )
 
-// const get = async (theme) => {
-//   const res = await fetch('https://newspast.vercel.app/api/db?theme=' + theme)// + '&secret=' + process.env.SECRET_TOKEN)
-//   return await res.json()
-// }
+const get = async (theme) => {
+  const res = await fetch('https://newspast.vercel.app/api/db?theme=' + theme)// + '&secret=' + process.env.SECRET_TOKEN)
+  return await res.json()
+}
 
 export default serve(inngest, [ getNews ])
