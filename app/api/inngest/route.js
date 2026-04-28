f1d5bf0e4f0373bd78f07166b7049d1812f28754
Denis <godlin086@gmail.com>
2026-04-28 07:14:09 +0300

    fix inngest function

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,13 @@
 import { Inngest } from 'inngest'
 import { serve } from 'inngest/next'
 
-export const inngest = new Inngest({ id: 'newspast' })
+const inngest = new Inngest({ id: 'newspast' })
 
-const getNews = inngest.createFunction(
-  { name: 'Get news' }, 
-  { cron: '0 */6 * * *' }, 
-  async ({ event, step }) => {
+const getNews = inngest.createFunction({ 
+    id: 'Get news', 
+    triggers: { cron: '0 */6 * * *' }
+  }, 
+  async ({ step }) => {
     await step.run('db world', async () => { return await get('world') })
     await step.run('db nation', async () => { return await get('nation') })
     await step.run('db scitech', async () => { return await get('scitech') })
